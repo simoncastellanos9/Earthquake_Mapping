0df61862908e8ec2642930300e088ed409265456
Simon Castellanos <simoncastellanos9@gmail.com>
2021-09-08 09:36:48 -0500

    added API key and layers

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
-SSSSS
+const API_KEY = "pk.eyJ1Ijoic2ltb25jYXN0ZWxsYW5vczkiLCJhIjoiY2t0Ymxqd2Z1MW1hNDJvbnVremFwdHRneSJ9.CP8qOIRWvX9Eig0ythJMZA";
---
+++ static/js/logic.js
@@ -0,0 +1,37 @@
+
+var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+});
+
+var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+});
+
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+});
+
+var map = L.map("map", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3,
+  layers: [graymap, satellitemap, outdoors]
+});
+
+graymap.addTo(map);
